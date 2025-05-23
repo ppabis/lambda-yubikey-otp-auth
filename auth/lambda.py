c78f7dbac83a9987c24352b811dbaa19d29aef50
ppabis <piotr@pabis.eu>
2025-05-23 08:30:19 +0200

    Lambda authorizer

---
+++ auth/lambda.py
@@ -0,0 +1,56 @@
+import re, yubico_client, os, base64
+from typing import Tuple
+
+CLIENT_ID = os.environ['CLIENT_ID']
+SECRET_KEY = os.environ['SECRET_KEY'] # In reality, this should be stored in AWS Secrets Manager!!!
+client = yubico_client.Yubico(CLIENT_ID, SECRET_KEY)
+
+def validate_otp(otp, client_id, secret_key) -> bool:
+  """Validate the OTP key using YubiCloud API"""
+  try:
+    response = client.Yubico(client_id, secret_key).verify(otp)
+    return True if response is True else False # Normalize in case it is None or anything unexpected
+  except Exception as e:
+    print(f"Error validating OTP: {e}")
+    return False
+
+def authenticate(token) -> Tuple[str, bool]:
+  """Authenticate the user using the OTP key. Returns tuple of public ID and if OTP is valid"""
+  if not token:
+    raise Exception("Missing Authorization header")
+  
+  # From "Basic base64(username:password)", get only "password"
+  otp = base64.b64decode(token.split(' ')[1]).decode('utf-8').split(':')[1]
+  
+  if not re.match(r'^[cbdefghijklnrtuv]{44}$', otp):
+    raise Exception("Invalid OTP format")
+  
+  return otp[:12], validate_otp(otp, CLIENT_ID, SECRET_KEY)
+
+def construct_policy(username, event):
+  _, _, _, region, accountId, apiGwPath = event['methodArn'].split(':')
+  restApiId, stage = apiGwPath.split('/')[:2]
+  return {
+    'principalId' : username,
+    'policyDocument' : {
+      'Version' : '2012-10-17',
+      'Statement' : [
+        {
+          'Action': 'execute-api:Invoke',
+          'Effect': 'Allow',
+          'Resource': [ f"arn:aws:execute-api:{region}:{accountId}:{restApiId}/{stage}/GET/*" ]
+        }
+      ]
+    }
+  }
+
+def lambda_handler(event, context):
+  try:
+    username, valid = authenticate(event['authorizationToken'])
+    if valid:
+      policy = construct_policy(username, event)
+      return policy
+  except Exception as e:
+    print(f"Error: {e}")
+    return "Unauthorized"
+  return "Unauthorized"
---
+++ lambda_auth.tf
@@ -0,0 +1,44 @@
+resource "null_resource" "pip_install" {
+  provisioner "local-exec" {
+    command = "pip install -r ${path.module}/auth/requirements.txt -t ${path.module}/auth/"
+    when    = create
+  }
+}
+
+resource "archive_file" "authorizer_lambda" {
+  type        = "zip"
+  source_dir  = "${path.module}/auth"
+  output_path = "${path.module}/authorizer.zip"
+
+  depends_on = [null_resource.pip_install]
+}
+
+resource "aws_iam_role" "lambda_otp_auth_role" {
+  name = "LambdaOtpAuthRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_otp_auth_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "otp_auth" {
+  function_name    = "otp_auth"
+  role             = aws_iam_role.lambda_otp_auth_role.arn
+  handler          = "lambda.lambda_handler"
+  runtime          = "python3.12"
+  timeout          = 15
+  memory_size      = 128
+  architectures    = ["arm64"]
+  filename         = archive_file.authorizer_lambda.output_path
+  source_code_hash = archive_file.authorizer_lambda.output_base64sha256
+}
