e47ee326f89ded711f9c5d100f2c3a0c58c51673
ppabis <piotr@pabis.eu>
2025-05-28 22:39:53 +0200

    Create OTP class
    Used for decoding, decryption and validation of YubiKey OTP

---
+++ auth/process_otp.py
@@ -0,0 +1,59 @@
+import yubico_client.modhex as modhex
+from Crypto.Cipher import AES
+
+class OTP:
+    def __init__(self, otp: str):
+        self.source_otp = otp
+        self.public_id = otp[:12]
+        m_private_part = otp[12:]
+        m_hex_private = list(modhex.translate(m_private_part, modhex.HEX))[0]
+        self.private_part = bytes.fromhex(m_hex_private)
+        self.decrypted = None
+        self.private_id = None
+        self.usage_counter = -1
+        self.timestamp = -1
+        self.session_counter = -1
+        self.combined_counter = -1
+        self.random = None
+        self.checksum = None
+
+    def decrypt(self, key: bytes):
+        self.decrypted = AES.new(key, AES.MODE_ECB).decrypt(self.private_part)
+        self.private_id = self.decrypted[:6]
+        self.usage_counter = int.from_bytes(self.decrypted[6:8], byteorder='little', signed=False)
+        self.timestamp = int.from_bytes(self.decrypted[8:11], byteorder='little', signed=False)
+        self.session_counter = int.from_bytes(self.decrypted[11:12], byteorder='little', signed=False)
+        self.random = self.decrypted[12:14]
+        self.checksum = self.decrypted[14:16]
+        self.combined_counter = self.usage_counter * 1000 + self.session_counter
+    
+    def validate(self, expected_private: bytes):
+        """
+        Source: https://github.com/Yubico/python-pyhsm/blob/master/pyhsm/soft_hsm.py#L132
+        """
+        m_crc = 0xffff
+        for this in self.decrypted:
+            m_crc ^= this
+            for _ in range(8):
+                j = m_crc & 1
+                m_crc >>= 1
+                if j:
+                    m_crc ^= 0x8408
+        
+        return m_crc == 0xf0b8 and expected_private == self.private_id
+    
+    def __str__(self):
+        if self.decrypted is None:
+            return f"""
+            {'Public ID:':<17} {self.public_id}
+            OTP not decrypted yet
+            """
+        return f"""
+        {'Public ID:':<17} {self.public_id}
+        {'Private ID:':<17} {self.private_id.hex(":")}
+        {'Usage Counter:':<17} {self.usage_counter}
+        {'Timestamp:':<17} {self.timestamp}
+        {'Session Counter:':<17} {self.session_counter}
+        {'Random:':<17} {self.random.hex(":")}
+        {'Checksum:':<17} {self.checksum.hex(":")}
+        """
