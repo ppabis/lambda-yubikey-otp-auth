0efe1e0b3748026fb211cd86da44f8cb6ab9965a
ppabis <piotr@pabis.eu>
2025-05-28 22:15:23 +0200

    Include missing secret.py file

---
+++ auth/secret.py
@@ -0,0 +1,6 @@
+import boto3, json
+
+def get_secret_manager_secret(secret_name):
+    client = boto3.client('secretsmanager')
+    response = client.get_secret_value(SecretId=secret_name)
+    return json.loads(response['SecretString'])
