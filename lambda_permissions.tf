4522e6891df442a810c70dfc7d032440fa3d8e09
ppabis <piotr@pabis.eu>
2025-05-29 20:45:59 +0200

    Formatting

--- lambda_permissions.tf
+++ lambda_permissions.tf
@@ -5,19 +5,19 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "lambda_otp_auth_policy" {
   statement {
     actions = [
-        "dynamodb:UpdateItem",
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:DeleteItem"
+      "dynamodb:UpdateItem",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
     ]
     resources = [aws_dynamodb_table.yubikey_otp_auth.arn]
   }
   statement {
     actions = ["secretsmanager:GetSecretValue"]
     resources = [
-        "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:yubikey-otp-*"
+      "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:yubikey-otp-*"
     ]
-  }  
+  }
 }
 
 resource "aws_iam_policy" "lambda_otp_auth_policy" {
